341045f2935b5921f289cfd393c08b28fd824c60
bhoiqr101 <141326163+bhoiqr101@users.noreply.github.com>
2024-05-05 16:05:04 +0530

    Update bypass.py

--- FZBypass/plugins/bypass.py
+++ FZBypass/plugins/bypass.py
@@ -99,7 +99,7 @@ async def bypass_check(client, message):
     if len(parse_data) != 0:
         parse_data[-1] = (
             parse_data[-1]
-            + f"┎ <b>Total Links : {no}</b>\n┠ <b>Results In <code>{convert_time(end - start)}</code></b> !\n┖ <b>By </b>{message.from_user.mention} ( #ID{message.from_user.id} )"
+            + f"┎ <b>Total Links : {no}</b>\n┠ <b>Results In <code>{convert_time(end - start)}</code></b> !\n┖ <b>By </b>{message.from_user.mention} ( #ID{message.from_user.id} )\n\nBy @gdhub_bypasser [⚡@GDHUBNATION]"
         )
     tg_txt = "━━━━━━━✦✗✦━━━━━━━\n\n"
     for tg_data in parse_data:
